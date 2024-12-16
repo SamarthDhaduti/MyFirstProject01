04c7053538add8ef9ff4d8df8994a140ed43c511
Samarth D <samarthcdhaduti@gmail.com>
2024-12-16 18:05:17 +0530

    FirstProject01

---
+++ src/main/java/com/example/firstproject/FirstProject.java
@@ -0,0 +1,22 @@
+package com.example.firstproject;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstProject {
+
+    @RequestMapping("/hello")
+    public String sayHello(){
+
+        return "Hello World";
+    }
+
+    @RequestMapping("/hello/{name}")
+    public String SayHelloToSomeone(@PathVariable("name") String name){
+
+        return "Hello Boss " + name;
+    }
+    
+}
